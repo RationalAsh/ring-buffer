3e89d8429faeedfd132bdab98a76597cf565c6ba
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2017-06-10 19:43:39 +0800

    Change

--- ring_buffer.c
+++ ring_buffer.c
@@ -2,22 +2,10 @@
 
 int init_ring_buffer(ring_buffer *rb, int max_len, size_t sz)
 {
-    rb->buffer = calloc(max_len, sz);
-    rb->head = rb->buffer;
-    rb->tail = rb->buffer;
-    rb->max_length = max_len;
-    rb->data_size = sz;
-    rb->status = STATUS_EMPTY;
+    
 }
 
 int push_data(ring_buffer *rb, void *data)
 {
-    if((rb->status == STATUS_EMPTY) || (rb->status == STATUS_PARTIALLY_FULL))
-    {
-        if(rb->tail)
-        *(rb->tail) = data;
-        rb->tail += rb->data_size;
-        rb->status = STATUS_PARTIALLY_FULL;
-        (rb->length)++;
-    }
+    
 }
